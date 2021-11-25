1333ba243af3ac1c3b133f3ff265521a83c52632
abdulahi roble <abdulahi_2610@hotmail.com>
2021-11-25 12:15:38 +0100

    first commit

---
+++ app/src/main/java/com/example/notesappfirebase/forgotpassword.java
@@ -0,0 +1,56 @@
+package com.example.notesappfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class forgotpassword extends AppCompatActivity {
+
+    private EditText mforgotpassword;
+    private Button mpasswordrecoverbutton;
+    private TextView mgobacktologin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgotpassword);
+
+        getSupportActionBar().hide();
+
+        mforgotpassword = findViewById(R.id.forgotpassword);
+        mpasswordrecoverbutton = findViewById(R.id.passwwordrecoverbutton);
+        mgobacktologin = findViewById(R.id.gobacktologin);
+
+        mgobacktologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(forgotpassword.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mpasswordrecoverbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail = mforgotpassword.getText().toString().trim();
+                if (mail.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(), "Enter your mail first", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    // we have to send recover email
+
+
+                }
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/notesappfirebase/signup.java
@@ -0,0 +1,62 @@
+package com.example.notesappfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class signup extends AppCompatActivity {
+
+    private EditText msignupemail, msignuppasword;
+    private RelativeLayout msignup;
+    private TextView mgotologin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        getSupportActionBar().hide();
+
+        msignupemail = findViewById(R.id.signupemail);
+        msignuppasword = findViewById(R.id.signuppassword);
+        msignup = findViewById(R.id.signup);
+        mgotologin = findViewById(R.id.gotologin);
+
+        mgotologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(signup.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        msignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String mail = msignupemail.getText().toString().trim();
+                String password = msignuppasword.getText().toString().trim();
+
+                if (mail.isEmpty() || password.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(), "All fields are required", Toast.LENGTH_SHORT).show();
+                }
+                else if (password.length() < 7)
+                {
+                    Toast.makeText(getApplicationContext(), "Password should be greater than 7 digits", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    // Register the user to to firebase
+                }
+            }
+        });
+
+    }
+}
